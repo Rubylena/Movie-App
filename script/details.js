25e4ad7755809ed90477d1194441b585498d79c8
Ruby <graceffiong@gmail.com>
2022-11-01 02:17:34 +0100

    add details page, download link

--- script/details.js
+++ script/details.js
@@ -9,16 +9,13 @@ const movieApp2 = async ()=>{
 
 async function movieDetails(){
     try{
-        // var html = '';
         const response = await fetch(url);
         const responseData = await response.json();
         const result = await responseData.data.movies;
-        console.log(result);
 
         const queryString = window.location.search;
         const parameters = new URLSearchParams(queryString);
         const movieId = parameters.get("id");
-        console.log(movieId);
 
         let movie = result.find((item) => {
             return item.id == movieId;
@@ -26,13 +23,45 @@ async function movieDetails(){
         console.log(movie);
 
         if (movieId) {
-            const movieDetailsContainer = document.createElement('div');
-            movieDetailsContainer.classList.add('movie_details_img');
+            const movieDetailsImage = document.createElement('div');
+            movieDetailsImage.classList.add('movie_details_img');
+            movieDetailsImage.style.flex = '30%';
             const img = document.createElement('img');
             img.src = movie.large_cover_image;
+            const content = document.createElement('div');
+            content.style.flex = '70%';
+            content.style.display = 'flex';
+            content.style.flexDirection = 'column';
+            content.style.gap = '1em';
+            content.style.padding = '0.5em';
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+            h1.style.fontSize = '2em';
+            const h2 = document.createElement('h2');
+            h2.textContent = movie.year;
+            const h3 = document.createElement('h3');
+            h3.textContent = movie.genres.join(' / ');
+            const p = document.createElement('p');
+            p.textContent = movie.summary;
+            const download = document.createElement('a');
+            download.href = movie.url;
+            download.textContent = 'Click Here to download'
+            download.classList.add('download');
+            const trailer = document.createElement('a');
+            trailer.textContent = 'Watch trailer'
+            trailer.href = `https://www.youtube.com/embed/${movie.yt_trailer_code}?rel=0&wmode=transparent&border=0&autoplay=1&iv_load_policy=3`;
+            trailer.style.textDecoration = 'none';
 
-            main.appendChild(movieDetailsContainer);
-            movieDetailsContainer.appendChild(img);
+
+            main.appendChild(movieDetailsImage);
+            movieDetailsImage.appendChild(img);
+            main.appendChild(content);
+            content.appendChild(h1);
+            content.appendChild(h2);
+            content.appendChild(h3);
+            content.appendChild(p);
+            content.appendChild(trailer);
+            content.appendChild(download);
     }
 
     } catch (error) {
